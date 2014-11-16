145c877db7576a6bb754fb1352e5507d7aff3826
gergalyb <gergalyb@gmail.com>
2014-11-16 14:54:13 +0100

    base layout

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Index</title>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+
+	<body>
+		<div id="banner">banner</div>
+		<div id="sidebar">
+			<ul id="menu">
+				<li id="menu_item">test1</li>
+				<li id="menu_item">test2</li>
+				<li id="menu_item">test3</li>
+				<li id="menu_item">test4</li>
+			</ul>
+		</div>
+		<div id="content">
+			content
+		</div>
+	</body>
+</html>
